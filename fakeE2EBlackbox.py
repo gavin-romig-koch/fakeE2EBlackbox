52c838d6c0c3c7cbed26ed3397649351ac86b3fb
Gavin Romig-Koch <gavin@redhat.com>
2018-10-11 11:20:42 -0400

    automatically change downaverage to test alerting
        downaverage is the percentage chance that this will report failure
        start with downaverage at 7%
        after 90 seconds change downaverage to 13%
        after 180 seconds change downaverage to 6%

--- fakeE2EBlackbox.py
+++ fakeE2EBlackbox.py
@@ -1,9 +1,12 @@
 from prometheus_client import start_http_server, Gauge, Counter, Summary
 import random
 import time
+import datetime
 
 INSIGHTS_E2E_UP = Gauge('insights_e2e_blackbox_up', 'Blackbox e2e works')
 INSIGHTS_E2E_SUCCESS = Summary('insights_e2e_blackbox_success', 'Blackbox e2e success')
+
+starttime = datetime.datetime.now()
 
 def process_request():
     scale = 100
@@ -14,7 +17,19 @@ def process_request():
     seconds = (r * 2) + (random.random() * (r * 2))
     print("sleeping for ",seconds)
     time.sleep(seconds)
-    if random.random() < 0.3:
+
+    totalupseconds = (datetime.datetime.now()-starttime).total_seconds()
+    print("total up seconds ",totalupseconds)
+
+    if totalupseconds < 90:
+        downaverage = 0.07
+    elif totalupseconds < 180:
+        downaverage = 0.13
+    else:
+        downaverage = 0.06
+    print("downaverage ",downaverage)
+
+    if random.random() < downaverage:
         print("down")
         INSIGHTS_E2E_UP.set(0)
         INSIGHTS_E2E_SUCCESS.observe(0)
